6a7fafb4c98ae53d9ae0401b11acde726dde9034
ismail-la <lahbariismail@gmail.com>
2024-01-30 01:03:41 +0100

    0x00. Pagination

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-A function named index_range that takes two integer arguments page and page_size.
+A function named that takes two integer arguments page and page_size.
 -return a tuple of size two containing a start index and an end index
 corresponding to the range of indexes to return in a list for those particular
 pagination parameters.
