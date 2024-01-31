f305db2e3c1bb50662f7ac6882a922c138996d6a
ismail-la <lahbariismail@gmail.com>
2024-01-31 19:27:30 +0100

    0x01. Caching

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -34,10 +34,8 @@ class LIFOCache(BaseCaching):
         self.cache_data[key] = item
         self.cache_data.move_to_end(key, last=True)
 
-     def get(self, key):
+    def get(self, key):
         """
-        Get an item by key
+        Get an item by key.
         """
-        if key is None or key not in self.cache_data.keys():
-            return None
-        return self.cache_data.get(key)
+        return self.cache_data.get(key, None)
