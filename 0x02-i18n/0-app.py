c5379cc5fda8b14a3e7068d943740118595440b0
ismail-la <lahbariismail@gmail.com>
2024-02-07 02:40:25 +0100

    0x02. i18n

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Basic Flask application.
+"""
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def get_index() -> str:
+    """The home/index page (Renders a basic html template)
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
